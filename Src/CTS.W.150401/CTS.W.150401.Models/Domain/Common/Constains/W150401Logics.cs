08185debe736262f3d465b86c9c8195ed55146c4
tnt1411 <tantn.it@gmail.com>
2015-05-10 11:37:09 +0700

    #Add add new folder

---
+++ Src/CTS.W.150401/CTS.W.150401.Models/Domain/Common/Constains/W150401Logics.cs
@@ -0,0 +1,7 @@
+﻿
+namespace CTS.W._150401.Models.Domain.Common.Constains
+{
+    class W150401Logics
+    {
+    }
+}
---
+++ Src/CTS.W.150401/CTS.W.150401.Models/Domain/Object/Admin/Items/ItemObject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CTS.W._150401.Models.Domain.Object.Admin.Items
+{
+    class ItemObject
+    {
+    }
+}
