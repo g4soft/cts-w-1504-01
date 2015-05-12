54644fba10dbcda4ea6359792b57f7460f4fe696
tnt1411 <tantn.it@gmail.com>
2015-05-13 06:25:12 +0700

    #Add Khởi tạo trang sản phẩm

--- Src/CTS.W.150401/CTS.W.150401.Models/Domain/Common/Constains/W150401Logics.cs
+++ Src/CTS.W.150401/CTS.W.150401.Models/Domain/Common/Constains/W150401Logics.cs
@@ -1,7 +1,10 @@
 ﻿
 namespace CTS.W._150401.Models.Domain.Common.Constains
 {
-    class W150401Logics
+    public static class W150401Logics
     {
+        public const string CD_PARAM_CD_SHOWBADGE = "01";
+        public const string CD_PARAM_CD_PAGERLIMIT = "02";
+        public const string CD_PARAM_CD_PAGERLIMIT_ADM = "03";
     }
 }
